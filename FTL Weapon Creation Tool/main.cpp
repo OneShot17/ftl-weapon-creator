19f54771d16885d4e8910c18fe22d61f724a2761
Stephen Brimhall <stephen.brimhall@icloud.com>
2014-07-05 21:38:30 -0700

    Initial Commit

---
+++ FTL Weapon Creation Tool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FTL Weapon Creation Tool
+//
+//  Created by Stephen Brimhall on 7/5/14.
+//  Copyright (c) 2014 Belman Industries. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
