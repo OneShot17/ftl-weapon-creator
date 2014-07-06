ce229f0432e92c135f80a0d0cf4b834b6f870dd8
Stephen Brimhall <stephen.brimhall@icloud.com>
2014-07-05 22:10:52 -0700

    Added extensive comments, fixed blueprints file path bug, added power writing

--- FTL Weapon Creation Tool/main.cpp
+++ FTL Weapon Creation Tool/main.cpp
@@ -17,14 +17,18 @@ void writeOpenTag(string,ofstream &);
 void writeCloseTag(string,ofstream &);
 
 int main(int argc, const char * argv[]) {
+    // Variable initialization
     string path = "";
     string pwd = "";
     string weaponName = "";
     int weaponTypeNum = 0;
     string weaponType = "";
     string shortName = "";
+    // Get path of folder that will contain mod contents
     cout << "Path of folder to create:\n";
     getline(cin,path);
+    
+    // Make the mod directory structure
     string command = "mkdir \"" + path + "\"";
     system(command.c_str());
     command = "mkdir \"" + path + "/data\"";
@@ -33,8 +37,11 @@ int main(int argc, const char * argv[]) {
     system(command.c_str());
     command = "mkdir \"" + path + "/audio\"";
     system(command.c_str());
+    
+    // Get the weapon name
     cout << "Weapon name: ";
     getline(cin,weaponName);
+    // Set weapon-blueprint name based on weapon name (so it's easy to find in ship editors xD)
     char blueprintName[weaponName.length()];
     for(int i = 0; weaponName.c_str()[i] != '\0'; i++) {
         if(weaponName.c_str()[i] == ' ') {
@@ -43,8 +50,11 @@ int main(int argc, const char * argv[]) {
         }
         blueprintName[i] = weaponName.c_str()[i];
     }
+    
+    // Set short name of weapon
     cout << "Short name: ";
     getline(cin,shortName);
+    // Get weapon type (NUMBER REQD!)
     cout << "Types:\nBeam (1)\nMissile (2)\nLaser (3)\nBomb (4)\nPick one: ";
     cin >> weaponTypeNum;
     switch (weaponTypeNum) {
@@ -61,54 +71,88 @@ int main(int argc, const char * argv[]) {
             weaponType = "BOMB";
             break;
     }
+    
+    // CD to data directory and open blueprints.xml.append in append mode, creating it if it doesn't exist
     pwd = path + "/data";
     command = "cd \"" + pwd + "\"";
-    path = pwd + "blueprints.xml.append";
+    path = pwd + "/blueprints.xml.append";
     system(command.c_str());
-    static ofstream out(path,ios::app);
+    ofstream out(path,ios::app);
+    
+    /////////////////////////////
+    // BEGIN BLUEPRINT WRITING //
+    /////////////////////////////
+    
+    // Create blueprint open tag
     string tag = "weaponBlueprint name=\"";
     tag += blueprintName;
     tag += "\"";
     writeOpenTag(tag,out);
+    // Create weapon type tag
     tag = "type";
     writeTag(tag, weaponType, out);
+    // Create weapon title tag
     tag = "title";
     writeTag(tag, weaponName, out);
+    // Create short name tag
     tag = "short";
     writeTag(tag,shortName, out);
+    // Ask for description and write tag to file
     cout << "Descripton: ";
     string desc;
     getline(cin, desc);
     tag = "desc";
     writeTag(tag,desc,out);
+    // Ask for tooltip and write tag to file
     cout << "Tooltip: ";
     string tooltip;
     getline(cin, tooltip);
     tag = "tooltip";
     writeTag(tag,tooltip,out);
+    // Ask for damage value and write tag to file
     cout << "Damage: ";
     string damage;
     tag = "damage";
     getline(cin,damage);
     writeTag(tag,damage,out);
+    // Ask for shield piercing value and write tag to file
     cout << "Shield piercing: ";
     string sp;
     getline(cin,sp);
     tag = "sp";
     writeTag(tag,sp,out);
+    // Ask for fire chance and write tag to file
     cout << "Fire chance: ";
     string fireChance;
     tag = "fireChance";
     getline(cin,fireChance);
     writeTag(tag,fireChance,out);
+    // Ask for cooldown and write tag to file
     cout << "Charge time: ";
     string cooldown;
     getline(cin,cooldown);
     tag = "cooldown";
     writeTag(tag,cooldown,out);
+    // Ask for power requirement and write tag to file
+    cout << "Power requirement: ";
+    string power;
+    tag = "power";
+    getline(cin,power);
+    writeTag(tag,power,out);
+    
+    // Code that still needs writing
+    
+    // Close weapon blueprint file
     tag = "weaponBlueprint";
     writeCloseTag(tag,out);
     out << endl << endl;
+    out.close();
+    
+    //////////////////////////////////
+    // BEGIN ANIMATION FILE WRITING //
+    //////////////////////////////////
+    
+    
     return 0;
 }
 
